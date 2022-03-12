50ab8bbca374f69a5f6b543eb034d9b0cd914204
Scott Wilder <53450610+Scott-Wilder@users.noreply.github.com>
2022-03-12 18:34:24 -0500

    style: Feature and updates related to styling

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ var output2 = document.getElementById("demo2");
 output.innerHTML = slider.value;
 output2.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
+// Update the current slider value and update slider background color (each time you drag the slider handle)
 slider.oninput = function () {
   output.innerHTML = this.value * 2;
   output2.innerHTML = this.value / 3.125;
+  var x = slider.value;
+  var color =
+    "linear-gradient(90deg, hsl(174, 77%, 80%)" +
+    x +
+    "%, hsl(224, 65%, 95%)" +
+    x +
+    "%)";
+  slider.style.background = color;
 };
